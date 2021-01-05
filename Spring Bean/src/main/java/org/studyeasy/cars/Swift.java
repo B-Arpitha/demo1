3f706f24b12f5a5696424e1a945d032069518b57
B-Arpitha <71487061+B-Arpitha@users.noreply.github.com>
2021-01-05 21:27:52 +0530

    sample example

--- Spring Bean/src/main/java/org/studyeasy/cars/Swift.java
+++ Spring Bean/src/main/java/org/studyeasy/cars/Swift.java
@@ -6,7 +6,12 @@ import org.studyeasy.interfaces.Car;
 public class Swift implements Car {
 
 	public String specs() {
+		System.out.println("hi");
 		return "Hatchback from Suzuki";
+	}
+	public String specs1() {
+		System.out.println("hdfvgdi");
+		return "Hatchback from zsdbcfjsd";
 	}
 
 }
