64fdf0f26eb17ed336b2fdd4d15fbff849a5bdca
B-Arpitha <71487061+B-Arpitha@users.noreply.github.com>
2021-01-05 21:34:57 +0530

    2nd commit

--- Spring Bean/src/main/java/org/studyeasy/cars/Swift.java
+++ Spring Bean/src/main/java/org/studyeasy/cars/Swift.java
@@ -6,12 +6,8 @@ import org.studyeasy.interfaces.Car;
 public class Swift implements Car {
 
 	public String specs() {
-		System.out.println("hi");
-		return "Hatchback from Suzuki";
-	}
-	public String specs1() {
 		System.out.println("hdfvgdi");
-		return "Hatchback from zsdbcfjsd";
+		return "abc";
 	}
 
 }
